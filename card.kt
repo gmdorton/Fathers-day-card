fc881bbb76fdef72f4486abce623efddb3e0c146
gmdorton <102552253+gmdorton@users.noreply.github.com>
2022-05-30 11:16:01 -0500

    Create card.kt
    Simple text Fathher's day card

---
+++ card.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val age = 9
+    val name = "Don"
+    println("Happy Father's Day, ${name}!")
+    println("You are the world's #1 dad")
+    println("For the past ${age} years.")
+    println("We love you!")
+
+    //Lets print a trophy
+    println(" ")
+    println("     _______________     ")
+    println(" ___|_______________|___ ")
+    println("|   |               |   |")
+    println("|   |               |   |")
+    println(" |  |               |  | ")
+    println("  |__|             |__|  ")
+    println("      |           |      ")
+    println("       |_________|       ")
+    println("        |       |        ")
+    println("        |       |        ")
+    println("        @@@@@@@@@        ")
+    println("        @@ #1  @@        ")
+    println("        @@ Dad @@        ")
+    println("        @@@@@@@@@        ")
+    println(" ")
+
+
+    //This prints an extra empty space for looks
+    println(" ")
+}
